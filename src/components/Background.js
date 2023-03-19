19ce3f40cb6fd772d8b6175c82aea073eb7a9cf2
Ryo Furukawa <furu.4se@gmail.com>
2023-03-19 13:32:56 +0900

    small fix

--- src/components/Background.js
+++ src/components/Background.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Background = () => {
   return (
-    <div className=' bg-white dark:bg-gradient-to-b from-slate-800 to-cyan-900 h-screen w-full -z-100 fixed top-0 transition druration-500'></div>
+    <div className=' bg-white dark:bg-slate-800 h-screen w-full -z-100 fixed top-0 transition druration-500'></div>
   )
 }
 
