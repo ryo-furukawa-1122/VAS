2320b0956ea70d07cdbb989387dbe30185be4fb3
Ryo Furukawa <furu.4se@gmail.com>
2023-03-20 15:19:37 +0900

    Setted the logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header className='flex justify-between py-4 px-3 top-0 shadow-sm sticky bg-slate-50 dark:bg-slate-900 bg-opacity-30 dark:bg-opacity-30 z-10 items-center'>
             <a href="/">
-                <img className='w-[70px] h-auto' src="./logo512.png" alt="logo"></img>
+                <img className='w-[70px] h-auto' src="./vasLogo.svg" alt="logo"></img>
             </a>
             <ThemeSwitch />
         </header>
